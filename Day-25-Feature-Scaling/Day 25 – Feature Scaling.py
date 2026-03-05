6638967513af8966e89b76a04fbb36f352e4211c
Mahesh Babu <159671368+MaheshBabu331@users.noreply.github.com>
2026-03-05 15:37:37 +0530

    Day 25: Implemented feature scaling using StandardScaler and MinMaxScaler with practical example

---
+++ Day-25-Feature-Scaling/Day 25 – Feature Scaling.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+data={
+  "age":[20,25,30],
+  "salary":[25000,30000,35000]
+}
+df=pd.DataFrame(data)
+#Standardization 
+scaler=StandardScaler()
+standardized= scaler.fit_transform(df)
+print("Standardized Data:")
+print(standardized)
+#Normalization 
+minmax=MinMaxScaler()
+normalized=minmax.fit_transform(df)
+print("Normalized Data")
+print(normalized)
