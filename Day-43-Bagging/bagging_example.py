128365a8332ed668c86ed126cc25b5c1cacd7ecf
Mahesh Babu <159671368+MaheshBabu331@users.noreply.github.com>
2026-03-23 19:56:52 +0530

    Day 43: Implemented Bagging using Decision Trees

---
+++ Day-43-Bagging/bagging_example.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+X, y = load_iris(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = BaggingClassifier(
+    estimator=DecisionTreeClassifier(),
+    n_estimators=10
+)
+
+model.fit(X_train, y_train)
+
+print("Accuracy:", model.score(X_test, y_test))
